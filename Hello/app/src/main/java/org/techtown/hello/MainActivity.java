2b30e051386fa32572c8c0dc0cf3ac9d99b3339b
DBSAMOR <dmsqlcejtk@gmail.com>
2018-08-06 14:52:45 +0900

    진행상황 정리. 스튜디오 버전 3.1.3 -> 3.2

---
+++ Hello/app/src/main/java/org/techtown/hello/MainActivity.java
@@ -0,0 +1,31 @@
+package org.techtown.hello;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.naver.com"));
+        startActivity(myIntent);
+    }
+
+    public void onButton2Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-0000-0000"));
+        startActivity(myIntent);
+    }
+
+    public void onButton3Clicked(View v){
+        Intent myIntent = new Intent(getApplicationContext(), MenuActivity.class);
+        startActivity(myIntent);
+    }
+}
